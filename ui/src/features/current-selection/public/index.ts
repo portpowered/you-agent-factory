1bc9715f4ed6f1b59ecca3944dec9edc0c790397
aabdi <andreas.abdi@outlook.com>
2026-05-26 17:11:19 +0700

    feat: [remove-current-selection-public-workstation-detail-re-exports-001] - [Remove redundant workstation-detail exports from the current-selection public barrel]

--- ui/src/features/current-selection/public/index.ts
+++ ui/src/features/current-selection/public/index.ts
@@ -1,7 +1,5 @@
 export * from "../components/current-selection-cards";
 export * from "../components/current-selection-widget";
-export * from "../messages/workstation-detail";
-export * from "../messages/workstation-detail-types";
 export * from "../state/selection-types";
 export * from "../hooks/useCurrentSelection";
 export * from "../hooks/useCurrentSelectionDetails";
