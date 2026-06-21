edcc4eb976ddc022a86e3688814f594a660eab24
Andreas Abdi <andreas.abdi@outlook.com>
2026-06-21 20:06:55 +0700

    update runner to codex (#861)
    * update runner to codex
    
    * Fix subsystem lint gates

--- pkg/factory/subsystems/subsystem_transitioner.go
+++ pkg/factory/subsystems/subsystem_transitioner.go
@@ -17,8 +17,6 @@ import (
 	"github.com/portpowered/infinite-you/pkg/factory/workstationconfig"
 	"github.com/portpowered/infinite-you/pkg/interfaces"
 	"github.com/portpowered/infinite-you/pkg/logging"
-	"github.com/portpowered/infinite-you/pkg/packagedfactories/goal"
-	"github.com/portpowered/infinite-you/pkg/packagedfactories/tts"
 	"github.com/portpowered/infinite-you/pkg/petri"
 	workerprovider "github.com/portpowered/infinite-you/pkg/workers/provider"
 )
@@ -930,72 +928,5 @@ func firstNonResourceInput(inputs []interfaces.TokenColor) *interfaces.TokenColo
 			return &inputs[i]
 		}
 	}
-	return nil
-}
-
-func applyPackagedTTSInvocationMetadata(
-	token *interfaces.Token,
-	workstation *interfaces.FactoryWorkstationConfig,
-	workerOutput string,
-	inputColors []interfaces.TokenColor,
-	runtimeConfig interfaces.RuntimeWorkstationLookup,
-) error {
-	if token == nil || !tts.ShouldFormatInvocationMetadata(workstation) {
-		return nil
-	}
-
-	traceID := ""
-	if source := firstNonResourceInput(inputColors); source != nil {
-		traceID = strings.TrimSpace(source.TraceID)
-	}
-
-	backendLabel := ""
-	if workstation != nil && runtimeConfig != nil {
-		if lookup, ok := runtimeConfig.(interfaces.RuntimeDefinitionLookup); ok {
-			if worker, ok := lookup.Worker(strings.TrimSpace(workstation.WorkerTypeName)); ok && worker != nil {
-				backendLabel = tts.BackendLabelFromWorker(worker)
-			}
-		}
-	}
-
-	metadataContent, err := tts.MetadataContentFromWorkerOutput(workerOutput, traceID, "", backendLabel)
-	if err != nil {
-		return fmt.Errorf("shape packaged tts invocation metadata: %w", err)
-	}
-
-	token.Color.Content = metadataContent
-	token.Color.Payload = nil
-	return nil
-}
-
-func applyPackagedGoalInvocationSummary(
-	token *interfaces.Token,
-	workstation *interfaces.FactoryWorkstationConfig,
-	workerOutput string,
-	runtimeConfig interfaces.RuntimeWorkstationLookup,
-) error {
-	if token == nil || !goal.ShouldFormatInvocationSummary(workstation) {
-		return nil
-	}
-	if strings.TrimSpace(workerOutput) == "" {
-		return nil
-	}
-
-	stopToken := ""
-	if workstation != nil && runtimeConfig != nil {
-		if lookup, ok := runtimeConfig.(interfaces.RuntimeDefinitionLookup); ok {
-			if worker, ok := lookup.Worker(strings.TrimSpace(workstation.WorkerTypeName)); ok && worker != nil {
-				stopToken = strings.TrimSpace(worker.StopToken)
-			}
-		}
-	}
-
-	summaryContent, err := goal.SummaryContentFromWorkerOutput(workerOutput, stopToken)
-	if err != nil {
-		return fmt.Errorf("shape packaged goal invocation summary: %w", err)
-	}
-
-	token.Color.Content = summaryContent
-	token.Color.Payload = nil
 	return nil
 }
