4bcbe30265b44a6a20a9d73c23a223606daf1373
aabdi <andreas.abdi@outlook.com>
2026-07-16 06:30:54 -0700

    fix: remove unused classifier metadata helper

--- pkg/factory/packages/classifier/metadata.go
+++ pkg/factory/packages/classifier/metadata.go
@@ -1,12 +1,7 @@
 // Package classifier owns the @you/classifier packaged-factory identity and topology metadata.
 package classifier
 
-import (
-	"strings"
-
-	builtinclassifier "github.com/portpowered/infinite-you/pkg/factory/packages/definitions/classifier"
-	"github.com/portpowered/infinite-you/pkg/interfaces"
-)
+import builtinclassifier "github.com/portpowered/infinite-you/pkg/factory/packages/definitions/classifier"
 
 // BuiltInFactoryJSON is the canonical runnable @you/classifier definition.
 var BuiltInFactoryJSON = builtinclassifier.BuiltInFactoryJSON
@@ -16,12 +11,3 @@ const (
 	PackagedFactoryProject = "builtin-classifier"
 	ClassifierWorkstation  = "classify-complexity"
 )
-
-// IsPackagedFactory reports whether cfg identifies the built-in classifier factory.
-func IsPackagedFactory(cfg *interfaces.FactoryConfig) bool {
-	if cfg == nil {
-		return false
-	}
-	return strings.TrimSpace(cfg.Name) == PackagedFactoryName ||
-		strings.TrimSpace(cfg.Project) == PackagedFactoryProject
-}
