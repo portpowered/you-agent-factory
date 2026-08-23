d01c9e38f6ed3c154fa63b5570bacffd6e390c96
aabdi <andreas.abdi@outlook.com>
2026-08-23 00:31:04 -0700

    deadcode: remove unreachable worker session CLI paths

--- pkg/services/worker_sessions/transports/cli/service.go
+++ pkg/services/worker_sessions/transports/cli/service.go
@@ -38,27 +38,6 @@ func selectEffects(effects []Effects) Effects {
 	return effects[0]
 }
 
-// Service exposes Worker Sessions CLI operations to Cobra composition.
-type Service interface {
-	List(ListConfig) error
-	Show(ShowConfig) error
-	Read(ReadConfig) error
-	Continue(ContinueConfig) error
-}
-
-type service struct{}
-
-// New constructs the Worker Sessions CLI adapter.
-func New() Service { return service{} }
-
-func (service) List(config ListConfig) error { return list(config) }
-
-func (service) Show(config ShowConfig) error { return show(config) }
-
-func (service) Read(config ReadConfig) error { return read(config) }
-
-func (service) Continue(config ContinueConfig) error { return continueWorkerSession(config) }
-
 func readInvokeRequest(config InvokeConfig) (factoryapi.WorkerSessionStartRequest, error) {
 	decoded, err := readInvokeRequestWithDiagnostics(config)
 	return decoded.Request, err
